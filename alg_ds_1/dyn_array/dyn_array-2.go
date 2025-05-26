07c27a36d9d49fc6fd453355faccdb49a794af80
Vladislav Morozov <landaren11@gmail.com>
2025-05-26 19:00:57 +0300

    dynamic array + tests + tests rename

---
+++ alg_ds_1/dyn_array/dyn_array-2.go
@@ -0,0 +1,173 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"os"
+)
+
+type BankArray[T any] struct {
+	array    []T
+	capacity int
+	size     int
+}
+
+func NewBankArray[T any]() *BankArray[T] {
+	return &BankArray[T]{
+		array:    make([]T, 1),
+		capacity: 1,
+		size:     0,
+	}
+}
+
+func (ba *BankArray[T]) Append(item T) {
+	if ba.size == ba.capacity {
+		newArray := make([]T, ba.capacity*2)
+		copy(newArray, ba.array)
+		ba.array = newArray
+		ba.capacity *= 2
+	}
+	ba.array[ba.size] = item
+	ba.size++
+}
+
+func (ba *BankArray[T]) Get(index int) (T, error) {
+	var zero T
+	if index < 0 || index >= ba.size {
+		return zero, fmt.Errorf("index out of bounds")
+	}
+	return ba.array[index], nil
+}
+
+func (ba *BankArray[T]) Remove(index int) error {
+	if index < 0 || index >= ba.size {
+		return fmt.Errorf("index out of bounds")
+	}
+	copy(ba.array[index:], ba.array[index+1:ba.size])
+	ba.size--
+
+	if ba.size <= ba.capacity/4 {
+		newCapacity := ba.capacity / 2
+		if newCapacity < 1 {
+			newCapacity = 1
+		}
+		newArray := make([]T, newCapacity)
+		copy(newArray, ba.array[:ba.size])
+		ba.array = newArray
+		ba.capacity = newCapacity
+	}
+	return nil
+}
+
+type MultiArray[T any] struct {
+	dimensions int
+	shape      []int
+	data       []T
+}
+
+func NewMultiArray[T any](dimensions int, shape ...int) (*MultiArray[T], error) {
+	if dimensions != len(shape) {
+		return nil, errors.New("dimensions count doesn't match shape length")
+	}
+
+	totalSize := 1
+	for _, size := range shape {
+		if size <= 0 {
+			return nil, errors.New("dimension size must be positive")
+		}
+		totalSize *= size
+	}
+
+	return &MultiArray[T]{
+		dimensions: dimensions,
+		shape:      shape,
+		data:       make([]T, totalSize),
+	}, nil
+}
+
+func (ma *MultiArray[T]) Get(indices ...int) (T, error) {
+	var zero T
+	if len(indices) != ma.dimensions {
+		return zero, errors.New("wrong number of indices")
+	}
+
+	index := 0
+	stride := 1
+	for i := ma.dimensions - 1; i >= 0; i-- {
+		if indices[i] < 0 || indices[i] >= ma.shape[i] {
+			return zero, fmt.Errorf("index %d out of bounds for dimension %d", indices[i], i)
+		}
+		index += indices[i] * stride
+		stride *= ma.shape[i]
+	}
+
+	return ma.data[index], nil
+}
+
+func (ma *MultiArray[T]) Set(value T, indices ...int) error {
+	if len(indices) != ma.dimensions {
+		return errors.New("wrong number of indices")
+	}
+
+	index := 0
+	stride := 1
+	for i := ma.dimensions - 1; i >= 0; i-- {
+		if indices[i] < 0 || indices[i] >= ma.shape[i] {
+			return fmt.Errorf("index %d out of bounds for dimension %d", indices[i], i)
+		}
+		index += indices[i] * stride
+		stride *= ma.shape[i]
+	}
+
+	ma.data[index] = value
+	return nil
+}
+
+func (ma *MultiArray[T]) Resize(newShape ...int) error {
+	if len(newShape) != ma.dimensions {
+		return errors.New("wrong number of dimensions")
+	}
+
+	newTotalSize := 1
+	for _, size := range newShape {
+		if size <= 0 {
+			return errors.New("dimension size must be positive")
+		}
+		newTotalSize *= size
+	}
+
+	newData := make([]T, newTotalSize)
+
+	oldIndices := make([]int, ma.dimensions)
+	for i := 0; i < len(ma.data); i++ {
+		copyIndices := true
+		for j := 0; j < ma.dimensions; j++ {
+			if oldIndices[j] >= newShape[j] {
+				copyIndices = false
+				break
+			}
+		}
+
+		if copyIndices {
+			newIndex := 0
+			stride := 1
+			for k := ma.dimensions - 1; k >= 0; k-- {
+				newIndex += oldIndices[k] * stride
+				stride *= newShape[k]
+			}
+			newData[newIndex] = ma.data[i]
+		}
+
+		for j := ma.dimensions - 1; j >= 0; j-- {
+			oldIndices[j]++
+			if oldIndices[j] < ma.shape[j] {
+				break
+			}
+			oldIndices[j] = 0
+		}
+	}
+
+	ma.data = newData
+	ma.shape = newShape
+	return nil
+}
---
+++ alg_ds_1/dyn_array/dyn_array.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+const (
+	increaseArraySize = 2
+	decreaseArraySize = 1.5
+
+	decreasePercent = 0.5
+)
+
+type DynArray[T any] struct {
+	count    int
+	capacity int
+	array    []T
+}
+
+func (da *DynArray[T]) Init() {
+	da.count = 0
+	da.MakeArray(16)
+}
+
+func (da *DynArray[T]) MakeArray(sz int) {
+	if sz < 16 {
+		sz = 16
+	}
+	var arr = make([]T, sz)
+	if da.count != 0 {
+		copy(arr, da.array)
+	}
+	da.capacity = sz
+	da.array = arr
+}
+
+func (da *DynArray[T]) Insert(itm T, index int) error {
+	if index > da.count || index < 0 {
+		return fmt.Errorf("index '%d' out of range", index)
+	}
+
+	if index == da.count {
+		da.Append(itm)
+		return nil
+	}
+	if da.capacity == da.count {
+		da.MakeArray(da.capacity * increaseArraySize)
+	}
+
+	for i := da.count; i > index; i-- {
+		if i != 0 {
+			da.array[i] = da.array[i-1]
+		}
+	}
+
+	da.array[index] = itm
+	da.count++
+	return nil
+}
+
+func (da *DynArray[T]) Remove(index int) error {
+	if index > da.count || index < 0 {
+		return fmt.Errorf("index '%d' out of range", index)
+	}
+	for i := index; i < da.count-1; i++ {
+		da.array[i] = da.array[i+1]
+	}
+	da.count--
+	decreasedCapacity := int(float64(da.capacity) * decreasePercent)
+	if da.count < decreasedCapacity {
+		newSize := int(float64(da.capacity) / decreaseArraySize)
+
+		da.MakeArray(newSize)
+	}
+	return nil
+}
+
+func (da *DynArray[T]) Append(itm T) {
+	if da.capacity == da.count {
+		da.MakeArray(da.capacity * increaseArraySize)
+	}
+
+	da.array[da.count] = itm
+	da.count++
+}
+
+func (da *DynArray[T]) GetItem(index int) (T, error) {
+	var result T
+	if index >= da.count || index < 0 {
+		return result, fmt.Errorf("index out of range %d", index)
+	}
+	result = da.array[index]
+	return result, nil
+}
