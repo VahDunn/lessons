628e7499761cfe17f28f7bf7d39d43f4d6b49205
Vladislav Morozov <landaren11@gmail.com>
2025-07-03 16:01:29 +0300

    naming

---
+++ clean_code/var names.go
@@ -0,0 +1,42 @@
+
+names - user_names_and_surnames_list
+// имена и фамилии пользователей
+
+ids - archive_report_ids_list
+// ID отчетов из архива
+
+files - lab_files_from_archives_list
+// файлы лабораторных исследований, относящиеся к архивам
+
+is_doc - document_exists_bool
+//булево значение - результат проверки на существование документа
+
+source - source_db_for_archive_str
+// база данных, в которой ищется архив
+
+ru - request_url_str
+// URL из запроса
+
+time - executed_at_ts
+// таймштамп выполнения запроса
+
+timedelta - execution_timedelta
+// длительность выполнения
+
+name - document_name_str
+// имя документа
+
+mi - request_match_info
+// match info из запроса
+
+ctx - global_context
+// глобальный контекст сервиса
+
+svc - service_name_str
+// имя сервиса
+
+// аналогичные примеры есть с параметрами функций, но там они сопровождаются тайпхинтами
+
+
+
+
