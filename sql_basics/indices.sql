c05594ca22a0ab298ec8fd1f0e83cf571fbedd3e
Vladislav Morozov <landaren11@gmail.com>
2025-08-28 12:44:36 +0300

    indices

--- sql_basics/indices.sql
+++ sql_basics/indices.sql
@@ -1,5 +1,14 @@
 PK - кластерный индекс на Region.RegionID
 
-PK - кластерный индекс на Territories.TerritoryID (кластерный)
+ALTER TABLE Region
+    ADD CONSTRAINT PK_Region PRIMARY KEY CLUSTERED (RegionID);
+
+PK - кластерный индекс на Territories.TerritoryID
+
+ALTER TABLE Territories
+    ADD CONSTRAINT PK_Territories PRIMARY KEY CLUSTERED (TerritoryID);
 
 Некластерный индекс на Territories.RegionID
+
+CREATE INDEX IX_Territories_RegionID
+ON Territories (RegionID);
