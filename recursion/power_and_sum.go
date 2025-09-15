245952f97ff8db1a864b38cdfb7c4e8e58a04a0e
Vladislav Morozov <landaren11@gmail.com>
2025-09-15 14:54:18 +0300

    Sum My Power

---
+++ recursion/power_and_sum.go
@@ -0,0 +1,21 @@
+package main
+
+func toPower(n int, m int) int {
+	if m == 0 {
+		return 1
+	}
+	if m < 0 {
+		return 1 / toPower(n, -m)
+	}
+	return n * toPower(n, m-1)
+}
+
+func digitSum(n int) int {
+	if n < 0 {
+		n = -n
+	}
+	if n < 10 {
+		return n
+	}
+	return n%10 + digitSum(n/10)
+}
