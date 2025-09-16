7452b710ea5bd834bcb3b03533186b87adc0a918
Vladislav Morozov <landaren11@gmail.com>
2025-09-16 15:02:44 +0300

    list + palindrome

--- recursion.go
+++ recursion.go
@@ -1,5 +1,10 @@
 package lessons
 
+import (
+	"strings"
+	"unicode"
+)
+
 func toPower(n int, m int) int {
 	if m == 0 {
 		return 1
@@ -18,4 +23,31 @@ func digitSum(n int) int {
 		return n
 	}
 	return n%10 + digitSum(n/10)
+}
+
+func listLength(ll []int) int {
+	if len(ll) == 0 {
+		return 0
+	}
+	return listLength(ll[1:]) + 1
+}
+
+func isPalindromeRec(s string) bool {
+	var filtered []rune
+	for _, r := range strings.ToLower(s) {
+		if unicode.IsLetter(r) || unicode.IsDigit(r) {
+			filtered = append(filtered, r)
+		}
+	}
+	return check(filtered)
+}
+
+func check(rs []rune) bool {
+	if len(rs) <= 1 {
+		return true
+	}
+	if rs[0] != rs[len(rs)-1] {
+		return false
+	}
+	return check(rs[1 : len(rs)-1])
 }
